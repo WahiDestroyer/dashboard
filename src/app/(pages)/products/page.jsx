5482d13cb05b9bfaeff4f26e9224a9e30a84f04c
Wahid Tahosin Laam <laamwahidtahosin@gmail.com>
2025-11-25 10:02:22 +0600

    hmm

--- src/app/(pages)/products/page.jsx
+++ src/app/(pages)/products/page.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      this is products page
+    <div className="mx-6">
+      <div>
+        <div>
+          <h2 className="font-poppins text-3xl text-white font-bold leading-8 pb-3.5">All Products</h2>
+          <div className="flex">
+            <button></button>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
-
+export default page;
